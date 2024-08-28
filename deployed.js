5c78ba7eacaf8072e805150e8a91e712a2051b61
Thomas Fung <59012417+thom008@users.noreply.github.com>
2024-08-29 08:58:51 +1000

    Update deployed.js

--- deployed.js
+++ deployed.js
@@ -1,5 +1,3 @@
-// authenticates you with the API standard library
-// type `await lib.` to display API autocomplete
 const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
 
 await lib.discord.channels['@0.2.0'].messages.create({
